edc1777252d5c401d18a6eecb47a23ec635d71ac
Victor Sampaio <vsampaio@pm.me>
2020-10-24 17:07:44 -0300

    Added a CustomAppBar widget

---
+++ lib/widgets/CustomAppBar.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppBar({
+    Key key,
+    @required String title,
+  })  : _title = title,
+        super(key: key);
+
+  @override
+  Size get preferredSize => const Size.fromHeight(100);
+
+  final String _title;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      elevation: 0,
+      title: Text(
+        _title,
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 24,
+        ),
+      ),
+      backgroundColor: Colors.white,
+      centerTitle: true,
+      leading: IconButton(
+        icon: Icon(Icons.menu),
+        color: Colors.black,
+        onPressed: () {},
+      ),
+    );
+  }
+}
