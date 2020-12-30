fbb233da9d793d220bc7d4123c0859b5894759f8
Victor Sampaio <vsampaio@pm.me>
2020-12-30 18:09:52 -0300

    Removing unwanted files and modifying books table

--- lib/database/Book.dart
+++ lib/database/Book.dart
@@ -6,15 +6,20 @@ class Book {
   final int year;
   final DateTime date;
   final int rating;
+  final int reading;
+  final int toRead;
 
-  Book(
-      {this.id,
-      this.cover,
-      this.title,
-      this.author,
-      this.year,
-      this.date,
-      this.rating});
+  Book({
+    this.id,
+    this.cover,
+    this.title,
+    this.author,
+    this.year,
+    this.date,
+    this.rating,
+    this.reading,
+    this.toRead,
+  });
 
   Map<String, dynamic> toMap() {
     return {
@@ -24,12 +29,14 @@ class Book {
       'author': author,
       'year': year,
       'date': date,
-      'rating': rating
+      'rating': rating,
+      'reading': reading,
+      'to_read': toRead,
     };
   }
 
   @override
   String toString() {
-    return 'Book{id: $id, cover: $cover, title: $title, author: $author, year: $year, date: $date, rating: $rating}';
+    return 'Book{id: $id, cover: $cover, title: $title, author: $author, year: $year, date: $date, rating: $rating, reading: $reading, to_read: $toRead}';
   }
 }
--- lib/database/BookDatabaseHelper.dart
+++ lib/database/BookDatabaseHelper.dart
@@ -3,56 +3,75 @@ import 'package:sqflite/sqflite.dart';
 import 'Book.dart';
 
 class BookDatabaseHelper {
-  final Future<Database> database = openDatabase(
-    join(await getDatabasesPath(), 'book_database.db'),
-    onCreate: (db, version) {
-      return db.execute(
-        "CREATE TABLE books(id INTEGER PRIMARY KEY, cover TEXT, title TEXT, author TEXT, year INTEGER, date DATETIME, rating INTEGER)",
-      );
-    },
-    version: 1,
-  );
+  static BookDatabaseHelper helper = BookDatabaseHelper._createInstance();
+  static Database _db;
+  int _version = 1;
+  BookDatabaseHelper._createInstance();
+
+  Future<Database> get db async {
+    if (_db == null) {
+      _db = await initializeDatabase();
+    }
+    return _db;
+  }
+
+  Future<Database> initializeDatabase() async {
+    return await openDatabase(
+      join(await getDatabasesPath(), 'book_database.db'),
+      onCreate: (db, version) {
+        return db.execute(
+          "CREATE TABLE books(id INTEGER PRIMARY KEY, cover TEXT, title TEXT, author TEXT, year INTEGER, date DATETIME, rating INTEGER, reading INTEGER, to_read INTEGER)",
+        );
+      },
+      version: _version,
+    );
+  }
 
   Future<void> insertBook(Book book) async {
-    final Database db = await database;
+    final Database db = _db;
     await db.insert(
       'Books',
-      Book.toMap(),
+      book.toMap(),
       conflictAlgorithm: ConflictAlgorithm.replace,
     );
   }
 
   Future<List<Book>> books() async {
-    final Database db = await database;
+    final Database db = _db;
     final List<Map<String, dynamic>> maps = await db.query('Books');
     return List.generate(maps.length, (i) {
       return Book(
-          id: maps[i]['id'],
-          cover: maps[i]['cover'],
-          title: maps[i]['title'],
-          author: maps[i]['author'],
-          year: maps[i]['year'],
-          date: maps[i]['date'],
-          rating: maps[i]['rating']);
+        id: maps[i]['id'],
+        cover: maps[i]['cover'],
+        title: maps[i]['title'],
+        author: maps[i]['author'],
+        year: maps[i]['year'],
+        date: maps[i]['date'],
+        rating: maps[i]['rating'],
+      );
     });
   }
 
-  Future<void> updateBook(Book book) async {
-    final db = await database;
-    await db.update(
+  Future<int> updateBook(Book book) async {
+    final db = _db;
+    final res = await db.update(
       'books',
-      Book.toMap(),
+      book.toMap(),
       where: "id = ?",
-      whereArgs: [Book.id],
+      whereArgs: [book.id],
     );
+
+    return res;
   }
 
-  Future<void> deleteBook(int id) async {
-    final db = await database;
-    await db.delete(
+  Future<int> deleteBook(int id) async {
+    final db = _db;
+    final res = await db.delete(
       'books',
       where: "id = ?",
       whereArgs: [id],
     );
+
+    return res;
   }
 }
